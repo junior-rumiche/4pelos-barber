4798f03ec855e7d3ce6e2b8bb4ca8fb27d76750e
Junior Rumiche <juniorrumichem@gmail.com>
2025-10-05 01:42:07 -0500

    feat: actualizar widgets en el panel de administración y modificar la columna de datos a JSON

--- database/migrations/2025_09_28_041938_create_notifications_table.php
+++ database/migrations/2025_09_28_041938_create_notifications_table.php
@@ -9,17 +9,30 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    // public function up(): void
+    // {
+    //     Schema::create('notifications', function (Blueprint $table) {
+    //         $table->uuid('id')->primary();
+    //         $table->string('type');
+    //         $table->morphs('notifiable');
+    //         $table->text('data');
+    //         $table->timestamp('read_at')->nullable();
+    //         $table->timestamps();
+    //     });
+    // }
+
     public function up(): void
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('type');
             $table->morphs('notifiable');
-            $table->text('data');
+            $table->json('data'); // This is the key line for PostgreSQL
             $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,8 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::redirect('/', '/admin')
+    ->name('home');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
